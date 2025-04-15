10382e7b0fea6d07494bfd1468662f6ef01f7057
k-behrens <78392060+k-behrens@users.noreply.github.com>
2025-04-14 20:36:57 -1000

    Update models.py

--- apps/epg/models.py
+++ apps/epg/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django_celery_beat.models import PeriodicTask
+from .utils import enhance_mlb_description
 
 class EPGSource(models.Model):
     SOURCE_TYPE_CHOICES = [
@@ -61,3 +62,21 @@ class ProgramData(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.start_time} - {self.end_time})"
+
+class Program(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if "mlb" in self.category.lower():
+            program_data = {
+                "title": self.title,
+                "desc": self.description,
+                "category": self.category,
+                "start": self.start_time.strftime("%Y%m%d%H%M%S")
+            }
+            enhanced = enhance_mlb_description(program_data)
+            self.description = enhanced["desc"]
+        super().save(*args, **kwargs)
